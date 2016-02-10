0e20f03b4310724005899d19059bf604f24f3650
kaiomagalhaes <contato@kaiomagalhaes.com.br>
2016-02-10 15:46:29 -0200

    setup style to footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return (
-      <div className="Footer">
+      <div className="footer">
         <div className="container">
           <span className="text">© Kaio Magalhães - {new Date().getFullYear()}</span>
         </div>
